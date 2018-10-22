74b4c57525a17728265dc72c50fc7a7da7e85f96
Germon97 <42594669+Germon97@users.noreply.github.com>
2018-10-21 21:24:46 -0400

    DIG3480 Challenge 2
    Mario level 1 challenge

---
+++ Mario 1-1/Assets/Scipts/BoxController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour {
+
+    private Animator anim;
+
+
+
+
+    //Wall check
+    private bool wallHit;
+    public Transform wallHitBox;
+    public float wallHitHeight;
+    public float wallHitWidth;
+    public LayerMask isGround;
+    public GameObject pickup;
+    private int i;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+            if (wallHit == true)
+            {
+                Instantiate(pickup);
+
+        }
+    }
+  
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallHitBox.position, new Vector3(wallHitWidth, wallHitHeight, 1));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+ 
---
+++ Mario 1-1/Assets/Scipts/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+
+    private Animator anim;
+
+    public float speed;
+
+
+    //Wall check
+    private bool wallHit;
+    public Transform wallHitBox;
+    public float wallHitHeight;
+    public float wallHitWidth;
+    public LayerMask isGround;
+
+    private bool headHit;
+    public Transform headHitBox;
+    public float headHitHeight;
+    public float headHitWidth;
+    public LayerMask isPlayer;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+        if (wallHit == true)
+        {
+            speed = speed * -1;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        headHit = Physics2D.OverlapBox(headHitBox.position, new Vector2(headHitWidth, headHitHeight), 0, isPlayer);
+        if (headHit == true)
+        {
+
+            Debug.Log("I loved living");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallHitBox.position, new Vector3(wallHitWidth, wallHitHeight, 1));
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(headHitBox.position, new Vector3(headHitWidth, headHitHeight, 1));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
